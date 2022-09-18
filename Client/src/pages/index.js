079c8df153d5e7f228badba4416b8f21cb32790b
omar214 <omarmohamed2322000@gmail.com>
2022-09-18 17:31:37 +0200

    pages structure

---
+++ Client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from './Home.jsx';
+export { default as Cart } from './Cart.jsx';
+export { default as Orders } from './Orders.jsx';
+export { default as Products } from './Products.jsx';
+export { default as Register } from './Register.jsx';
+export { default as Signup } from './Signup.jsx';
