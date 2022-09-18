cc710ab486dc9a5d0991f68da43e5f46f09b7fb3
omar214 <omarmohamed2322000@gmail.com>
2022-09-18 19:55:10 +0200

    Home Page Done

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
+import data from '../data/data.js';
+import { Product } from '../components';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<Container>
+			<h2> Featured Products </h2>
+			<Row>
+				{/* TODO make dynamic products */}
+				{data.products.map((p) => (
+					<Col sm={6} md={4} lg={3} className="mb-3" key={p.slug}>
+						<Product p={p} />
+					</Col>
+				))}
+			</Row>
+		</Container>
+	);
 };
 
 export default Home;
--- Client/src/pages/index.js
+++ Client/src/pages/index.js
@@ -2,5 +2,5 @@ export { default as Home } from './Home.jsx';
 export { default as Cart } from './Cart.jsx';
 export { default as Orders } from './Orders.jsx';
 export { default as Products } from './Products.jsx';
-export { default as Register } from './Register.jsx';
+export { default as Login } from './Login.jsx';
 export { default as Signup } from './Signup.jsx';
