0d72db7afde3be07c52027ef20c646ad1617aec8
omar214 <omarmohamed2322000@gmail.com>
2022-09-19 10:17:08 +0200

    edit cart styles

--- Client/src/pages/Cart.jsx
+++ Client/src/pages/Cart.jsx
@@ -5,7 +5,6 @@ import Alert from 'react-bootstrap/Alert';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
-import Stack from 'react-bootstrap/Stack';
 import data from '../data/data.js';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
@@ -18,16 +17,16 @@ const Cart = () => {
 	// const p = data.products[0];
 
 	return (
-		<Container>
+		<Container className="pb-4">
 			<h2 className="mb-4">Shopping Cart</h2>
 			<Row>
-				<Col sm={12} md={8}>
+				<Col lg={8}>
 					{cartCount > 0 ? (
 						<ListGroup>
 							{data.products.map((p) => (
 								<ListGroup.Item>
-									<Row className="align-items-center">
-										<Col sm={4} className="me-1 ">
+									<Row className="align-items-center text-center text-md-left">
+										<Col md={4} className="me-1">
 											<Image
 												src={p.image}
 												className="img-thumbnail me-2"
@@ -37,22 +36,20 @@ const Cart = () => {
 											<Link to={`/products/${p.name}`}>{p.name}</Link>
 										</Col>
 
-										<Col sm={4} className="me-1 ">
-											<Stack direction="horizontal" gap={1}>
-												<Button size="sm" variant="light">
-													<RemoveIcon />
-												</Button>
-												{cartCount}
-												<Button size="sm" variant="light">
-													<AddBoxIcon />
-												</Button>
-											</Stack>
+										<Col md={4} className="me-1 ">
+											<Button size="sm" variant="light">
+												<AddBoxIcon />
+											</Button>
+											{cartCount}
+											<Button size="sm" variant="light">
+												<RemoveIcon />
+											</Button>
 										</Col>
 
 										<Col className="me-1 ">
-											<strong>{p.price}</strong>
+											<strong>${p.price}</strong>
 										</Col>
-										<Col className="me-1 ">
+										<Col className="">
 											<DeleteIcon />
 										</Col>
 									</Row>
@@ -65,7 +62,7 @@ const Cart = () => {
 						</Alert>
 					)}
 				</Col>
-				<Col sm={12} md={4} className="mt-5 mt-md-0">
+				<Col lg={4} className="mt-5 mt-lg-0">
 					<ListGroup>
 						<ListGroup.Item className="p-sm-4 text-center mt-sm-4 mt-md-0">
 							<h4>Subtotal ({cartCount} items) : $0</h4>
