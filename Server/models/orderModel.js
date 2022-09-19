8155c85be2db826bbac4c7191e7ae05e1251d6e6
omar214 <omarmohamed2322000@gmail.com>
2022-09-19 20:55:17 +0200

    order Model

---
+++ Server/models/orderModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+var subProduct = mongoose.Schema(
+	{
+		quantity: {
+			type: Number,
+			default: 1,
+		},
+		product: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Product',
+		},
+	},
+	{ _id: false },
+);
+
+const orderSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: [true, 'userId is required'],
+		},
+		items: {
+			type: [subProduct],
+		},
+		totalPrice: {
+			type: Number,
+			default: 0,
+		},
+		isPaid: {
+			type: Boolean,
+			default: false,
+		},
+		isDelivered: {
+			type: Boolean,
+			default: false,
+		},
+		address: {
+			type: String,
+			default: 'user address',
+		},
+	},
+	{ timestamps: true },
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
