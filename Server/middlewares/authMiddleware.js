793f9d9e60b1a07766068c27016080b93af9fdf3
omar214 <omarmohamed2322000@gmail.com>
2022-09-19 16:56:36 +0200

    add auth middleware

---
+++ Server/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import config from '../config/index.js';
+import createError from '../utils/createError.js';
+
+// Verify
+const verifyAuth = (req, res, next) => {
+	const token = req.headers.authorization;
+	const JWT_KEY = config.JWT_PASSWORD;
+
+	if (!token) next(createError(401, 'No token provided'));
+	try {
+		const decoded = jwt.verify(token, JWT_KEY);
+		req.userData = decoded;
+		next();
+	} catch (err) {
+		next(err);
+	}
+};
+
+export default verifyAuth;
