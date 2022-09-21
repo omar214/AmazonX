b391eb923b58e3a9a65797a09793436fb6e3672c
omar214 <omarmohamed2322000@gmail.com>
2022-09-21 19:41:25 +0200

    Shipping page Logic Done

--- Client/src/pages/Shipping.jsx
+++ Client/src/pages/Shipping.jsx
@@ -1,14 +1,48 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { CheckoutSteps } from '../components';
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAddress } from '../redux/cartSlice.js';
 
 const Shipping = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [errorMessage, setErrorMessage] = useState('');
+	const formRef = useRef(null);
+	const { address } = useSelector((state) => state.cart);
+
 	const handleSubmitAddress = (e) => {
 		e.preventDefault();
+
+		let name = formRef.current.name.value.trim(),
+			address = formRef.current.address.value.trim(),
+			city = formRef.current.city.value.trim(),
+			postalCode = formRef.current.postalCode.value.trim(),
+			country = formRef.current.country.value.trim();
+
+		setErrorMessage('');
+		if (!name || !address || !city || !postalCode || !country) {
+			setErrorMessage('missing field , Please Enter All Fields');
+			return;
+		}
+
+		dispatch(
+			setAddress({
+				name,
+				address,
+				postalCode,
+				city,
+				country,
+			}),
+		);
+		navigate('/payment');
 	};
 	return (
 		<Container>
@@ -16,28 +50,48 @@ const Shipping = () => {
 			<Row className="justify-content-center mb-5">
 				<Col md={6}>
 					<h2 className="mb-3">Shipping Address</h2>
-					<Form onSubmit={handleSubmitAddress}>
+					<Form onSubmit={handleSubmitAddress} ref={formRef}>
 						<FloatingLabel label="Full Name" className="mb-3">
-							<Form.Control type="email" placeholder="name@example.com" />
+							<Form.Control
+								type="text"
+								name="name"
+								defaultValue={address.name}
+							/>
 						</FloatingLabel>
 
 						<FloatingLabel label="Address" className="mb-3">
-							<Form.Control type="email" placeholder="name@example.com" />
+							<Form.Control
+								type="text"
+								defaultValue={address.address}
+								name="address"
+							/>
 						</FloatingLabel>
 
 						<FloatingLabel label="City" className="mb-3">
-							<Form.Control type="email" placeholder="name@example.com" />
+							<Form.Control
+								type="text"
+								defaultValue={address.city}
+								name="city"
+							/>
 						</FloatingLabel>
 
 						<FloatingLabel label="Postal Code" className="mb-3">
-							<Form.Control type="email" placeholder="name@example.com" />
+							<Form.Control
+								type="text"
+								defaultValue={address.postalCode}
+								name="postalCode"
+							/>
 						</FloatingLabel>
 
 						<FloatingLabel label="Country" className="mb-3">
-							<Form.Control type="email" placeholder="name@example.com" />
+							<Form.Control
+								type="text"
+								defaultValue={address.country}
+								name="country"
+							/>
 						</FloatingLabel>
+						{errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
 
-            {/* TODO check if submit done nav to payment */}
 						<Button type="submit"> Submit</Button>
 					</Form>
 				</Col>
--- Client/src/redux/cartSlice.js
+++ Client/src/redux/cartSlice.js
@@ -1,5 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getAddress = () => {
+	let temp;
+	if (localStorage.getItem('address')) {
+		temp = JSON.parse(localStorage.getItem('address'));
+	} else {
+		temp = {
+			name: '',
+			address: '',
+			city: '',
+			country: '',
+			postalCode: '',
+		};
+	}
+	return temp;
+};
+
 const initialState = {
 	cart: localStorage.getItem('cart')
 		? JSON.parse(localStorage.getItem('cart'))
@@ -7,6 +23,7 @@ const initialState = {
 	cartCount: localStorage.getItem('cart')
 		? JSON.parse(localStorage.getItem('cart')).items.length
 		: 0,
+	address: getAddress(),
 };
 
 export const cartSlice = createSlice({
@@ -18,14 +35,19 @@ export const cartSlice = createSlice({
 			state.cartCount = action.payload.items.length;
 			localStorage.setItem('cart', JSON.stringify(action.payload));
 		},
+		setAddress: (state, action) => {
+			state.address = action.payload;
+			localStorage.setItem('address', JSON.stringify(action.payload));
+		},
 		clearCart: (state) => {
 			localStorage.removeItem('cart');
 			state.cart = null;
 			state.cartCount = 0;
+			state.address = getAddress();
 		},
 	},
 });
 
-export const { setCartItems, clearCart } = cartSlice.actions;
+export const { setCartItems, clearCart, setAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
