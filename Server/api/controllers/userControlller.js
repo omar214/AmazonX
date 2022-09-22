c0b0de4d1919297a3c6e81eea467a378b6a51b6c
omar214 <omarmohamed2322000@gmail.com>
2022-09-22 20:57:36 +0200

    user profile route

--- Server/api/controllers/userControlller.js
+++ Server/api/controllers/userControlller.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import User from '../../models/userModel.js';
 import createError from '../../utils/createError.js';
 import config from '../../config/index.js';
-// import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 // import jwt from 'jsonwebtoken';
 
 const getAllUsers = async (req, res, next) => {
@@ -37,13 +37,28 @@ const updateUser = async (req, res, next) => {
 		if (!id || !mongoose.isValidObjectId(id))
 			return next(createError(401, 'valid id is required'));
 
-		let user = await User.findOne({ _id: id });
-		if (!user) return next(createError(404, 'user is not found '));
+		const { email, password } = req.body;
+		let user = await User.findOne({ email });
+		let myUser = await User.findOne({ _id: id });
+
+		if (user && myUser.email !== email)
+			return next(createError(400, 'Email Already in use'));
+		if (!myUser) return next(createError(404, 'user is not found '));
+
+		// not admin & not order owner
+		if (req.userData.id !== id && !req.userData.isAdmin)
+			return next(createError(403, 'you can only update your order'));
+
 		for (let key in req.body) {
-			user[key] = req.body[key];
+			myUser[key] = req.body[key];
 		}
-		user = await user.save();
-		res.status(200).json({ user });
+		if (password) myUser.password = bcrypt.hashSync(password);
+
+		myUser = await myUser.save();
+		res.status(200).json({
+			message: 'user updates successfully',
+			user: myUser,
+		});
 	} catch (error) {
 		next(error);
 	}
--- Server/api/routes/userRoute.js
+++ Server/api/routes/userRoute.js
@@ -4,12 +4,11 @@ import userController from '../controllers/userControlller.js';
 
 import { verifyAuth, verifyAdmin } from '../../middlewares/authMiddleware.js';
 
+router.put('/:id', verifyAuth, userController.updateUser);
 router.use(verifyAdmin);
-
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 
 router.delete('/:id', userController.deleteUser);
-router.put('/:id', userController.updateUser);
 
 export default router;
