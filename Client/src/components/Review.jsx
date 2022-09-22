e4cd603fa7956e4162d52a3c3dd4340772b58aaa
omar214 <omarmohamed2322000@gmail.com>
2022-09-22 22:54:49 +0200

    edit review style

--- Client/src/components/Review.jsx
+++ Client/src/components/Review.jsx
@@ -1,18 +1,22 @@
 import StarIcon from '@mui/icons-material/Star';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import { useEffect, useState } from 'react';
 
 const Review = ({ rating, numReviews }) => {
-	const stars = [];
-	for (let i = 1; i <= rating; i++) {
-		stars.push(<StarIcon key={i} fontSize="small" />);
-	}
-	if (rating !== Math.floor(rating)) {
-		stars.push(<StarHalfIcon key={6} fontSize="small" />);
-	}
-
 	return (
 		<>
-			<span className="text-primary">{stars}</span>
+			{[1, 2, 3, 4, 5].map((idx) => (
+				<span className="text-primary">
+					{rating === idx - 0.5 ? (
+						<StarHalfIcon key={6} fontSize="small" />
+					) : rating >= idx ? (
+						<StarIcon fontSize="small" />
+					) : (
+						<StarBorderIcon fontSize="small" />
+					)}
+				</span>
+			))}
 			{numReviews !== -1 && (
 				<p className="text-primary">{numReviews ?? 0} Reviews</p>
 			)}
