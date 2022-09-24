b58f365c598c7e8a45f65cb559608261b361a1b3
omar214 <omarmohamed2322000@gmail.com>
2022-09-24 18:54:30 +0200

    add footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -28,29 +28,39 @@ function App() {
 			deferLoading={true}
 			options={{ 'client-id': process.env.REACT_APP_PAYPAL_CLIENT_ID }}
 		>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="products/:id" element={<Product />} />
-					<Route path="cart" element={<Cart />} />
-					<Route path="login" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
-					<Route path="shipping" element={<Shipping />} />
-					<Route path="payment" element={<Payment />} />
-					<Route path="placeorder" element={<PlaceOrder />} />
-					<Route path="user" element={<UserProfile />} />
-					<Route path="orders" element={<Orders />} />
-					<Route path="orders/:id" element={<Order />} />
-					<Route path="search" element={<Search />} />
-					<Route path="admin/orders" element={<AdminOrders />} />
-					<Route path="admin/users" element={<AdminUsers />} />
-					<Route path="admin/users/:id" element={<AdminUserEdit />} />
-					<Route path="admin/products" element={<AdminProducts />} />
-					<Route path="admin/products/:id" element={<AdminProductEdit />} />
-					<Route path="admin/dashboard" element={<Dashboard />} />
-				</Routes>
-			</BrowserRouter>
+			<div className="d-flex flex-column body">
+				<main>
+					<BrowserRouter>
+						<Navbar />
+						<Routes>
+							<Route index element={<Home />} />
+							<Route path="products/:id" element={<Product />} />
+							<Route path="cart" element={<Cart />} />
+							<Route path="login" element={<Login />} />
+							<Route path="signup" element={<Signup />} />
+							<Route path="shipping" element={<Shipping />} />
+							<Route path="payment" element={<Payment />} />
+							<Route path="placeorder" element={<PlaceOrder />} />
+							<Route path="user" element={<UserProfile />} />
+							<Route path="orders" element={<Orders />} />
+							<Route path="orders/:id" element={<Order />} />
+							<Route path="search" element={<Search />} />
+							<Route path="admin/orders" element={<AdminOrders />} />
+							<Route path="admin/users" element={<AdminUsers />} />
+							<Route path="admin/users/:id" element={<AdminUserEdit />} />
+							<Route path="admin/products" element={<AdminProducts />} />
+							<Route path="admin/products/:id" element={<AdminProductEdit />} />
+							<Route path="admin/dashboard" element={<Dashboard />} />
+						</Routes>
+					</BrowserRouter>
+				</main>
+				<footer className="flex-shrink-0 py-4">
+					<div className="container text-center">
+						All Rights Reserved to{' '}
+						<a href="https://github.com/omar214">Omar214</a>
+					</div>
+				</footer>
+			</div>
 		</PayPalScriptProvider>
 	);
 }
